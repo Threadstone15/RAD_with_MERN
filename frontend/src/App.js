a12d128e3c4a76d9b01931034bd09e695d4ed57c
Pehan Ransika <140164973+pehanransika@users.noreply.github.com>
2024-08-15 21:36:12 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes ,useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
